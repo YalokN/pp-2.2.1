fe92c30184c2ffabe41b2b69c0239f6ceefc74d7
YalokN <118694186+YalokN@users.noreply.github.com>
2023-04-25 17:27:58 +0500

    Add files via upload

---
+++ pp-2-2-1/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,17 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+
+    void add(Car car);
+    List<User> listUsers();
+
+    List<User> getUserByCar(String model, int series);
+
+    List<Car> listCars();
+}
